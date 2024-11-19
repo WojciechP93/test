3300c14154c757c8dc0fad3e9505231dd5abc4db
Pelka Wojciech <wpelka@sii.pl>
2024-11-19 14:48:19 +0100

    add feature 2

--- TestConsoleApp/TestConsoleApp/Program.cs
+++ TestConsoleApp/TestConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Feacher1");
+Console.WriteLine("Feacher2");
 Console.WriteLine("Feacher3");
 Console.WriteLine("Feacher4");
 Console.WriteLine("Feacher5");
